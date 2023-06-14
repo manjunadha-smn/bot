aff6276332e6b12c9fdff667d2d3f85757cafaa2
manjunadha-smn <76811558+manjunadha-smn@users.noreply.github.com>
2023-06-14 15:10:51 +0530

    Update settings.py

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hah)!#vjt2(zp8#-0#gko@a+pbe2r*@5l5cyaap2pdko$%9by7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['43.204.109.137','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['52.66.250.207','localhost','0.0.0.0']
 
 
 # Application definition
